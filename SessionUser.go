69668f91feb0b85842757712e811aae91be71f1c
nordmanii <thomas.nordmann@itslearning.com>
2020-02-29 21:57:51 +0100

    Initial commit

---
+++ SessionUser.go
@@ -0,0 +1,29 @@
+package itszwizard_objects
+
+/*
+The Session User is the data which is stored in the cookie.
+It must be called gob.Register (itswizard_objects.SessionUser{})
+ */
+type SessionUser struct {
+	Username      		string
+	UserID          	uint
+	FirstAuthentication bool
+	Authenticated 		bool
+	TwoFac 				bool
+	Firstname			string
+	Lastname			string
+	Mobile				string
+	IpAddress       	string
+	Institution			string
+	School				string
+	Email 				string
+	Information 		string
+	Admin       		bool
+	OrganisationID		uint
+	InstitutionID       uint
+}
+
+func NewSessionUser () *SessionUser{
+	return new(SessionUser)
+}
+
