0658115f70d3d1bfd38430515715b2e1d33a1849
nordmanii <thomas.nordmann@itslearning.com>
2020-02-29 22:43:14 +0100

    delete testDBstruct

--- SessionUser.go
+++ SessionUser.go
@@ -3,27 +3,29 @@ package itszwizard_objects
 /*
 The Session User is the data which is stored in the cookie.
 It must be called gob.Register (itswizard_objects.SessionUser{})
- */
+*/
 type SessionUser struct {
-	Username      		string
-	UserID          	uint
+	Username            string
+	UserID              uint
 	FirstAuthentication bool
-	Authenticated 		bool
-	TwoFac 				bool
-	Firstname			string
-	Lastname			string
-	Mobile				string
-	IpAddress       	string
-	Institution			string
-	School				string
-	Email 				string
-	Information 		string
-	Admin       		bool
-	OrganisationID		uint
+	Authenticated       bool
+	TwoFac              bool
+	Firstname           string
+	Lastname            string
+	Mobile              string
+	IpAddress           string
+	Institution         string
+	School              string
+	Email               string
+	Information         string
+	Admin               bool
+	OrganisationID      uint
 	InstitutionID       uint
 }
 
-func NewSessionUser () *SessionUser{
+/*
+Creates a pointer to a SessionUser
+*/
+func NewSessionUser() *SessionUser {
 	return new(SessionUser)
 }
-
